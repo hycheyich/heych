004d90cb9bed429cabe249ad784458f1369084ff
。。。 <326393044@qq.com>
2019-11-14 10:47:49 +0800

    这是一次提交

--- text.c
+++ text.c
@@ -1,6 +1,19 @@
 #include <stdio.h>
 
-void main(){
-	print('这是一个hello word');
-	return NUll
+void main()
+{
+	return null;
 }
+
+int test(args,argc){
+	printf('1');
+	return 0;
+}
+void text()
+{
+	printf('222')
+		return null;
+}
+
+test()
+
